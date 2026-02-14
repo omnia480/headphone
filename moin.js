d1064ce3eb54997f87f7a9ddaa99782fe6c5e61f
omnia <omniashraff@gmail.com>
2026-02-14 18:00:09 +0200

    headphone
    website by HTML , Css ,gasp

---
+++ moin.js
@@ -0,0 +1,70 @@
+var tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".text1",
+        start: "0% 95%",
+        end: "80% 60%",
+        scrub: true,
+    }
+});
+tl.to("#headphone",{
+    top:"140%",
+    left:"77%",
+    width:"30%",
+    rotate: "-20deg",
+},'.headphone')
+var tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".text2",
+        start: "0% 95%",
+        end: "80% 60%",
+        scrub: true,
+    }
+});
+tl.to("#headphone",{
+    top:"220%",
+    left: '30%',
+    width:"30%",
+    rotate: "20deg",
+},'.headphone')
+var tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".text3",
+        start: "0% 95%",
+        end: "80% 60%",
+        scrub: true,
+    }
+});
+tl.to("#headphone",{
+    top:"282%",
+    left:"77%",
+    width:"30%",
+    rotate: "-20deg",
+},'.headphone')
+var tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".text4",
+        start: "0% 95%",
+        end: "80% 60%",
+        scrub: true,
+    }
+});
+tl.to("#headphone",{
+    top:"350%",
+    left:"30%",
+    width:"30%",
+    rotate: "20deg",
+},'.headphone')
+var tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".three",
+        start: "0% 95%",
+        end: "80% 60%",
+        scrub: true,
+    }
+});
+tl.to("#headphone",{
+    top:"470%",
+    left: "80%",
+    width:"40%",
+    rotate: "0deg",
+},'.headphone')
